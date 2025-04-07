64a7bc9bc299d829e44158076c26599b22793fa4
sarahosama.nabulsi <sarahosama.nabulsi@uzh.ch>
2025-04-07 14:03:35 +0200

    added type files, deck.ts and flashcard.ts

---
+++ app/types/deck.ts
@@ -0,0 +1,15 @@
+import { Flashcard } from "./flashcard";
+import { Invitation } from "./invitation";
+import { Quiz } from "./quiz";
+import { User } from "./user";
+
+export interface Deck {
+    id: string;
+    title: string | null;
+    flashcards: Flashcard | null;
+    deckCategory: string;
+    isPublic: boolean ;
+    user: User;
+    quiz: Quiz;
+    invitation: Invitation | null;
+}
---
+++ app/types/flashcard.ts
@@ -0,0 +1,13 @@
+import { Deck } from "./deck";
+
+export interface Flashcard {
+    id: string;
+    description: string | null;
+    date: Date;
+    answer: string;
+    wrongAnswers: string[];
+    imageUrl: string | null;
+    isPublic: boolean ;
+    flashcardCategory: string;
+    deck: Deck;
+}
